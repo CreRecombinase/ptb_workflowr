7c90698e85c2bf0733635c6b29c55b6a9655ca73
CreRecombinase <nwknoblauch@gmail.com>
2019-05-01 17:34:05 -0500

    tweaked config

--- R/files.R
+++ R/files.R
@@ -4,3 +4,9 @@ map_file = fs::path_expand(fs::path(data_config$KG_MAPDIR,"interpolated_EUR.h5")
 ldetect_file <- fs::path_expand(fs::path(data_config$LDETECT_DIR,"fourier_ls-all.tsv.gz"))
 kg_dir <-fs::path_expand(fs::path(data_config$KG_DIRECTORY,"h5/EUR/"))
 input_db_f <- fs::path_expand(fs::path(data_config$KG_DIRECTORY,"ga_gwas.db"))
+
+stopifnot(all(file.exists(c(
+                          map_file,
+                          ldetect_file,
+                          kg_dir,
+                          input_db_f))))
